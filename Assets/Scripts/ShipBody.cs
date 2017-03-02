e4d053595f1a778809350b4983fd9e1bc7392328
maiger <mage_ege@hotmail.com>
2017-03-02 17:34:32 +0200

    Add the basics of ship controls

---
+++ Assets/Scripts/ShipBody.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipBody : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Can set a custom center of mass for the object. Basically a pivot point when applying forces e.g. with thrusters")]
+    Transform centerOfMass;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		if(centerOfMass != null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+            rb.centerOfMass = centerOfMass.position;
+        }
+	}
+}
---
+++ Assets/Scripts/Thruster.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Thruster : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Button to activate thruster")]
+    KeyCode key;
+
+    [SerializeField]
+    [Tooltip("Force of thruster")]
+    float force = 10;
+
+    [SerializeField]
+    ParticleSystem jet;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        jet.Stop();
+
+	}
+
+    // FIX: Getting user input in fixedupdate will likely end up with input loss
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(key))
+        {
+            rb.AddForce(transform.up * force);
+            jet.Play();
+            //Debug.Log("Thrusting!");
+        }
+
+        if (Input.GetKeyUp(key))
+        {
+            jet.Stop();
+        }
+    }
+}
