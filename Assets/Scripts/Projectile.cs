683e7ddb7c717d9892034772f598b7d9e8ce2b42
maiger <mage_ege@hotmail.com>
2017-03-04 10:23:25 +0200

    Add weapon and a projectile
    Add a simple weapon for the ship
    Add a simple bullet projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Speed of projectile")]
+    int moveSpeed = 20;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        Destroy(gameObject, 1f);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Weapon : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Projectile the weapon uses")]
+    GameObject projectile;
+
+    [SerializeField]
+    [Tooltip("Point to spawn projectile")]
+    Transform firePoint;
+
+    [SerializeField]
+    [Tooltip("Force to be applied to weapon when fired")]
+    float force;
+
+    [SerializeField]
+    [Tooltip("Button to activate weapon")]
+    KeyCode key;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(key))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        Instantiate(projectile, firePoint.position, firePoint.rotation);
+        // Add force to the opposite direction of firing. This could be done better by
+        // making this depend on the projectile used and not a value on the weapon itself.
+        rb.AddForce(-transform.right * force);
+    }
+}
