a121aef105b00ae47883c6ccc38ab3b5c3886f3e
maiger <mage_ege@hotmail.com>
2017-03-06 18:29:53 +0200

    Start work on new input system
    Goal is to transfer all input handling from individual thrusters to the
    ship the thrusters belong to

--- Assets/Scripts/ShipBody.cs
+++ Assets/Scripts/ShipBody.cs
@@ -9,6 +9,9 @@ public class ShipBody : MonoBehaviour {
 
     private Rigidbody2D rb;
 
+    private Thruster[] thrusters;
+    private Weapon[] weapons;
+
 	// Use this for initialization
 	void Start () {
 		if(centerOfMass != null)
@@ -16,5 +19,20 @@ public class ShipBody : MonoBehaviour {
             rb = GetComponent<Rigidbody2D>();
             rb.centerOfMass = centerOfMass.position;
         }
-	}
+
+        // Thrusters are in the second child of the ship object
+        thrusters = transform.GetChild(1).GetComponentsInChildren<Thruster>();
+        Debug.Log("Thrusters found: " + thrusters.Length);
+
+        // Weapons are in the third child of the ship object
+        weapons = transform.GetChild(2).GetComponentsInChildren<Weapon>();
+        Debug.Log("Weapons found: " + weapons.Length);
+
+        // TODO: Sort through available thrusters and figure out where they are on the ship and what they do
+        // Which thursters move the ship forward, rotate, lateral movement, backwards?
+        // Could be calculated by referencing thruster position and ship center of mass position
+    }
+
+    // TODO: Update function
+    // Registers player inputs and calls appropriate thruster to turn on
 }
